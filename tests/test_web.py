04eb6c20264afc317cb13ae11eb215f986bf9c27
Vladimir Fesik <vladimir.fesik@kapowai.online>
2022-01-26 23:09:30 +0200

    allure

--- tests/test_web.py
+++ tests/test_web.py
@@ -1,12 +1,17 @@
+import allure
+
+@allure.story('epic_1')
+@allure.title("Parameterized test title")
 def test_true():
     """true"""
 
     assert True
 
-
+@allure.story('epic_2')
+@allure.feature('feature_2')
 def test_false():
     assert True
 
-
+@allure.feature('feature_3')
 def test_failed():
     assert False
